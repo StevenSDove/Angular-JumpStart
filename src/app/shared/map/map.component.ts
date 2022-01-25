83d9a6ae503870e1e5f40e5acdeb123c2cddb075
StevenSDove <steven.dove@gmail.com>
2022-01-25 12:29:01 -0500

    1 file modified

--- src/app/shared/map/map.component.ts
+++ src/app/shared/map/map.component.ts
@@ -1,8 +1,14 @@
-/// <reference path="../../../../node_modules/@types/googlemaps/index.d.ts" />
 
 import {
-  Component, OnInit, AfterContentInit, Input, ViewChild,
-  ContentChildren, ElementRef, QueryList, ChangeDetectionStrategy
+  Component,
+  OnInit,
+  AfterContentInit,
+  Input,
+  ViewChild,
+  ContentChildren,
+  ElementRef,
+  QueryList,
+  ChangeDetectionStrategy,
 } from '@angular/core';
 
 import { debounceTime } from 'rxjs/operators';
@@ -15,10 +21,9 @@ import { IMapDataPoint } from '../../shared/interfaces';
   // When using OnPush detectors, then the framework will check an OnPush
   // component when any of its input properties changes, when it fires
   // an event, or when an observable fires an event ~ Victor Savkin (Angular Team)
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MapComponent implements OnInit, AfterContentInit {
-
   private isEnabled: boolean = false;
   private loadingScript: boolean = false;
   private map: google.maps.Map = {} as google.maps.Map;
@@ -53,10 +58,12 @@ export class MapComponent implements OnInit, AfterContentInit {
     this.init();
   }
 
-  @ViewChild('mapContainer', { static: true }) mapDiv: ElementRef = {} as ElementRef;
-  @ContentChildren(MapPointComponent) mapPoints: QueryList<MapPointComponent> = {} as QueryList<MapPointComponent>;
+  @ViewChild('mapContainer', { static: true }) mapDiv: ElementRef =
+    {} as ElementRef;
+  @ContentChildren(MapPointComponent) mapPoints: QueryList<MapPointComponent> =
+    {} as QueryList<MapPointComponent>;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     if (this.latitude && this.longitude) {
@@ -64,7 +71,9 @@ export class MapComponent implements OnInit, AfterContentInit {
         this.mapHeight = this.height + 'px';
         this.mapWidth = this.width + 'px';
       } else {
-        const hw = this.getWindowHeightWidth(this.mapDiv.nativeElement.ownerDocument);
+        const hw = this.getWindowHeightWidth(
+          this.mapDiv.nativeElement.ownerDocument
+        );
         this.mapHeight = hw.height / 2 + 'px';
         this.mapWidth = hw.width + 'px';
       }
@@ -72,15 +81,11 @@ export class MapComponent implements OnInit, AfterContentInit {
   }
 
   ngAfterContentInit() {
-    this.mapPoints.changes
-        .pipe(
-          debounceTime(500)
-        )
-        .subscribe(() => {
-          if (this.enabled) { 
-            this.renderMapPoints(); 
-          }
-        });
+    this.mapPoints.changes.pipe(debounceTime(500)).subscribe(() => {
+      if (this.enabled) {
+        this.renderMapPoints();
+      }
+    });
   }
 
   init() {
@@ -92,15 +97,19 @@ export class MapComponent implements OnInit, AfterContentInit {
   }
 
   private getWindowHeightWidth(document: Document) {
-    let width = window.innerWidth
-      || document.documentElement.clientWidth
-      || document.body.clientWidth;
+    let width =
+      window.innerWidth ||
+      document.documentElement.clientWidth ||
+      document.body.clientWidth;
 
-    const height = window.innerHeight
-      || document.documentElement.clientHeight
-      || document.body.clientHeight;
+    const height =
+      window.innerHeight ||
+      document.documentElement.clientHeight ||
+      document.body.clientHeight;
 
-    if (width > 900) { width = 900; }
+    if (width > 900) {
+      width = 900;
+    }
 
     return { height: height, width: width };
   }
@@ -108,37 +117,50 @@ export class MapComponent implements OnInit, AfterContentInit {
   private ensureScript() {
     this.loadingScript = true;
     const document = this.mapDiv.nativeElement.ownerDocument;
-    const script = <HTMLScriptElement>document.querySelector('script[id="googlemaps"]');
+    const script = <HTMLScriptElement>(
+      document.querySelector('script[id="googlemaps"]')
+    );
     if (script) {
-      if (this.isEnabled) { this.renderMap(); }
+      if (this.isEnabled) {
+        this.renderMap();
+      }
     } else {
       const mapsScript = document.createElement('script');
       mapsScript.id = 'googlemaps';
       mapsScript.type = 'text/javascript';
       mapsScript.async = true;
       mapsScript.defer = true;
-      mapsScript.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCG1KDldeF_2GzaTXrEHR0l6cyCS7AnmBw';
+      mapsScript.src =
+        'https://maps.googleapis.com/maps/api/js?key=AIzaSyCG1KDldeF_2GzaTXrEHR0l6cyCS7AnmBw';
       mapsScript.onload = () => {
         this.loadingScript = false;
-        if (this.isEnabled) { this.renderMap(); }
+        if (this.isEnabled) {
+          this.renderMap();
+        }
       };
       document.body.appendChild(mapsScript);
     }
   }
 
   private renderMap() {
-    const latlng = this.createLatLong(this.latitude, this.longitude) as google.maps.LatLng;
+    const latlng = this.createLatLong(
+      this.latitude,
+      this.longitude
+    ) as google.maps.LatLng;
     const options = {
       zoom: this.zoom,
       center: latlng,
       mapTypeControl: true,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
     } as google.maps.MapOptions;
 
     this.map = new google.maps.Map(this.mapDiv.nativeElement, options);
 
     // See if we have any mapPoints (child content) or dataPoints (@Input property)
-    if ((this.mapPoints && this.mapPoints.length) || (this.dataPoints && this.dataPoints.length)) {
+    if (
+      (this.mapPoints && this.mapPoints.length) ||
+      (this.dataPoints && this.dataPoints.length)
+    ) {
       this.renderMapPoints();
     } else {
       this.createMarker(latlng, this.markerText);
@@ -146,7 +168,9 @@ export class MapComponent implements OnInit, AfterContentInit {
   }
 
   private createLatLong(latitude: number, longitude: number) {
-    return (latitude && longitude) ? new google.maps.LatLng(latitude, longitude) : null;
+    return latitude && longitude
+      ? new google.maps.LatLng(latitude, longitude)
+      : null;
   }
 
   private renderMapPoints() {
@@ -154,12 +178,20 @@ export class MapComponent implements OnInit, AfterContentInit {
       this.clearMapPoints();
 
       // lon/lat can be passed as child content or via the dataPoints @Input property
-      const mapPoints = (this.mapPoints && this.mapPoints.length) ? this.mapPoints : this.dataPoints;
+      const mapPoints =
+        this.mapPoints && this.mapPoints.length
+          ? this.mapPoints
+          : this.dataPoints;
 
       if (mapPoints) {
         for (const point of mapPoints) {
-          let markerText = (point.markerText) ? point.markerText : `<h3>${point.firstName} ${point.lastName}</h3>`;
-          const mapPointLatlng = this.createLatLong(point.latitude, point.longitude) as google.maps.LatLng;
+          let markerText = point.markerText
+            ? point.markerText
+            : `<h3>${point.firstName} ${point.lastName}</h3>`;
+          const mapPointLatlng = this.createLatLong(
+            point.latitude,
+            point.longitude
+          ) as google.maps.LatLng;
           this.createMarker(mapPointLatlng, markerText);
         }
       }
@@ -175,14 +207,14 @@ export class MapComponent implements OnInit, AfterContentInit {
 
   private createMarker(position: google.maps.LatLng, title: string) {
     const infowindow = new google.maps.InfoWindow({
-      content: title
+      content: title,
     });
 
     const marker = new google.maps.Marker({
       position: position,
       map: this.map,
       title: title,
-      animation: google.maps.Animation.DROP
+      animation: google.maps.Animation.DROP,
     });
 
     this.markers.push(marker);
